594b2927dc428ecdb24a04b4dde7ea1743cdc354
Kei <kei-k@pop-os.localdomain>
2023-07-18 20:48:01 +0630

    change code base and some style for responsive

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const mainContainer = document.getElementById("main__container");
 const loadingContainer = document.getElementById("loading__container");
+const form = document.getElementById("submitForm");
 
-// built url to fetch from API opentdb.com quiz's api
+// Build URL to fetch from OpenTDB API
 function buildURL() {
-  return `https://opentdb.com/api.php?amount=1&type=multiple`;
+  return "https://opentdb.com/api.php?amount=1&type=multiple";
 }
 
-// built question form data that response form the api
+// display question in labels
+function displayLabels(choice) {
+  for (let i = 1; i <= choice.length; i++) {
+    const choiceElement = document.getElementById(`choice_${i}`);
+    const labelEle = document.getElementById(`choice${i}`);
+    choiceElement.value = choice[i - 1];
+    labelEle.innerHTML = choice[i - 1];
+  }
+}
+
+// Build question from API response data
 function buildQuestion(question) {
-  // object destructured
   const {
     question: questionText,
     correct_answer,
@@ -17,19 +27,16 @@ function buildQuestion(question) {
 
   document.getElementById("question").innerHTML = questionText;
 
-  const choiceArray = randomQuestion(correct_answer, incorrect_answers);
+  const choiceArray = randomizeQuestion(correct_answer, incorrect_answers);
 
-  for (let i = 1; i <= choiceArray.length; i++) {
-    document.getElementById(`choice_${i}`).value = choiceArray[i - 1];
-    document.getElementById(`choice${i}`).innerHTML = choiceArray[i - 1];
-  }
+  displayLabels(choiceArray);
 }
 
-// function to generate the array that contain correct and incorrect answers
-function randomQuestion(correctAnswer, incorrectAnswers) {
+// Function to generate an array that contains correct and incorrect answers in randomized order
+function randomizeQuestion(correctAnswer, incorrectAnswers) {
   const randomArray = [correctAnswer, ...incorrectAnswers];
 
-  // shuffle the array to change the order of the element
+  // Shuffle the array to change the order of the elements
   for (let i = randomArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [randomArray[i], randomArray[j]] = [randomArray[j], randomArray[i]];
@@ -38,8 +45,9 @@ function randomQuestion(correctAnswer, incorrectAnswers) {
   return randomArray;
 }
 
-// async function that deal with API's call
+// Async function that handles API call
 async function callingAPI(url) {
+  // Show loading gif when data is being fetched
   mainContainer.style.display = "none";
   loadingContainer.style.display = "block";
 
@@ -56,37 +64,42 @@ async function callingAPI(url) {
     loadingContainer.style.display = "none";
   }
 }
-// async function that process api call function
+
+// Async function that processes the API call function
 async function processAPICall() {
-  const form = document.getElementById("submitForm");
   const reqURL = buildURL();
-  const correct_answer = await callingAPI(reqURL);
-  // form submit event and check the answer comparing with correct and user selected answer
+  const correctAnswer = await callingAPI(reqURL);
+
+  // Form submit event to check the answer against the correct and user-selected answer
+  checkAnswer(correctAnswer);
+}
+
+function checkAnswer(correctAns) {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const selectedAnswer = document.querySelector(
       'input[name="choices"]:checked'
     );
+
     if (selectedAnswer) {
       const selectedValue = selectedAnswer.value;
-      const correctAnswer = document.querySelector(
-        'input[value="' + correct_answer + '"]'
+      const correctAnswerElement = document.querySelector(
+        `input[value="${correctAns}"]`
       );
-      if (selectedValue === correctAnswer.value) {
-        document.getElementById("correct__alert").style.display =
-          "inline-block";
 
+      if (selectedValue === correctAnswerElement.value) {
+        displayAlert("correct");
         setTimeout(() => {
-          clearDisplayAlert("c");
+          clearDisplayAlert();
+          form.reset();
+          processAPICall();
         }, 1000);
-        form.reset();
-        processAPICall();
       } else {
-        document.getElementById("wrong__alert").style.display = "inline-block";
+        displayAlert("wrong");
         setTimeout(() => {
-          clearDisplayAlert("w");
+          clearDisplayAlert();
+          form.reset();
         }, 1000);
-        form.reset();
       }
     } else {
       alert("Please select an answer");
@@ -94,13 +107,20 @@ async function processAPICall() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", processAPICall());
+// Event listener to trigger API call when the DOM is loaded
+document.addEventListener("DOMContentLoaded", processAPICall);
 
-// function that clear the alert
-function clearDisplayAlert(status) {
-  if (status === "c") {
-    document.getElementById("correct__alert").style.display = "none";
-  } else if (status === "w") {
-    document.getElementById("wrong__alert").style.display = "none";
-  }
+// Function to display the alert
+function displayAlert(status) {
+  const alertElement = document.getElementById(`${status}__alert`);
+  alertElement.style.display = "inline-block";
+}
+
+// Function to clear the displayed alert
+function clearDisplayAlert() {
+  const correctAlert = document.getElementById("correct__alert");
+  const wrongAlert = document.getElementById("wrong__alert");
+
+  correctAlert.style.display = "none";
+  wrongAlert.style.display = "none";
 }
